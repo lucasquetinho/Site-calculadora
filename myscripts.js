92d44b89533501cc231b7faac6a4982432f7234d
lucasquetinho <105065904+lucasquetinho@users.noreply.github.com>
2023-10-26 12:15:31 -0300

    Uploud do dia 26 - aula de programação
    uploud do dia 26 - aula de programação

---
+++ myscripts.js
@@ -0,0 +1,6 @@
+function calcular() {}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const calcularButton = document.querySelector("#calcular");
+    calcularButton.addEventListener("click", calcular);
+});
